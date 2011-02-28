49177889414591c01a1aad352e8950552901d4ed
Alex Langenfeld <alangenfeld@wisc.edu>
2011-02-28 17:35:20 -0600

    Correct file this time.

--- chunk/chunkServer.go
+++ chunk/chunkServer.go
@@ -1,65 +1,61 @@
 package main
 
 import (
+	"http"
+	"rpc"
+	"os"
+	"log"
 	"net"
 	"fmt"
-	"flag"
-	"log"
-//	"time"
 )
 
+type Args struct {
+	ChunkID string
+	BlockOffset int
+}
+
+type Block struct {
+	Data [64 * 1024]byte
+}
+
+type Chunk struct {
+	Blocks [10]Block
+	Checksums [10]uint32
+}
+
+type BFS int
+
+const BLOCK_SIZE int = 1024 * 64
+const BLOCKS_PER_CHUNK int = 1024
+
+func (t *BFS) Read(args *Args, block *Block) os.Error {
+
+	file, err := os.Open(args.ChunkID, os.O_RDONLY, 0666)
+	if err != nil {
+		log.Fatal("os.Open error: ", err)
+	}
+
+//	_, err = file.ReadAt(block.Data[:], (int64)(args.BlockOffset*BLOCK_SIZE))
+	_, err = file.Read(block.Data[:])
+	if err != nil {
+		log.Fatal("file.Read error: ", err)
+	}
+
+	file.Close()
+
+	return nil
+}
 
 func main() {
 
-	servName := flag.Arg(0)
+	bfs := new(BFS)
+	rpc.Register(bfs)
+	rpc.HandleHTTP()
 
-	localAddr,err := net.ResolveUDPAddr(servName + ":1234")
-	if err != nil {
-		log.Fatal("UDP Resolve Failure: ", err)
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
 	}
-
-	local,err := net.ListenUDP("udp", localAddr)
-	if err != nil {
-		log.Fatal("UDP Listen Failure: ", err)
-	}
-
-	var buff [1]byte
-	var n int
-
-	n,remoteAddr,err := local.ReadFromUDP(buff[:])
-	if err != nil {
-		log.Fatal("listen ReadFromUDP: ", err, n)
-	}
-	
-	remote,err := net.DialUDP("udp", nil, remoteAddr)
-	if err != nil {
-		log.Fatal("DialUDP : ", err)
-	}
-	
-	//	fmt.Println("bytes read", n, "from",  remoteAddr)
-	
-	n,err = remote.WriteToUDP(buff[:], remoteAddr)
-	if err != nil {
-		log.Fatal("listen WriteFromUDP: ", err, n)
-	}
-	
-	for ;; {
-		n,remoteAddr,err := local.ReadFromUDP(buff[:])
-		if err != nil {
-			log.Fatal("listen ReadFromUDP: ", err, n)
-		}
-
-		remote,err := net.DialUDP("udp", nil, remoteAddr)
-		if err != nil {
-			log.Fatal("DialUDP : ", err)
-		}
-		
-		//	fmt.Println("bytes read", n, "from",  remoteAddr)
-		
-		n,err = remote.WriteToUDP(buff[:], remoteAddr)
-		if err != nil {
-			log.Fatal("listen WriteFromUDP: ", err, n)
-		}
-	}
-	fmt.Println("bytes written", n, "to", remoteAddr)
+	http.Serve(l, nil)
+	fmt.Println("done")
 }
