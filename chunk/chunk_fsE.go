260e57b63c250b9f9b731d70d770a500f79b6245
Elise <egale@cameron.(none)>
2011-02-28 19:42:57 -0600

    makefile update and new fs

---
+++ chunk/chunk_fsE.go
@@ -0,0 +1,39 @@
+package chunk
+
+import (
+	"../include/sfs" 
+	"log"
+	"os"
+	"strconv"
+	"container/list"
+)
+
+type Server int
+
+func (t *Server) Read(args *sfs.ReadArgs, chunk *sfs.Chunk) os.Error {
+
+	fileName := strconv.Uitob64(args.ChunkID, 10)
+	file, err := os.Open(fileName, os.O_RDONLY, 0666)
+	if err != nil {
+		log.Fatal("os.Open error: ", err)
+	}
+
+	_, err = file.Read(chunk.Data[:])
+	if err != nil {
+		log.Fatal("file.Read error: ", err)
+	}
+
+	file.Close()
+
+	return nil
+}
+
+func (t *Server) Heartbeat(args *sts.HeartbeatArgs, status *sfs.Status) os.Error {
+        return nil
+}
+
+func Init(status sfs.Status) {
+        status.ChunkCount = 0
+        status.ChunkIDs = ChunkIDs.New(List)
+        return
+}
