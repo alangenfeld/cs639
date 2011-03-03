051de0914b6fd80ee33ebc5fa0466e9e671a441f
Kevin Paprocki <kpaprocki@wisc.edu>
2011-03-03 17:19:20 -0600

    removed http rpc, changed a stupid name

---
+++ master/runmaster.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"./master"
+	"fmt"
+	"flag"
+	"rpc"
+	"http"
+	"net"
+	"log"
+)
+
+var host = flag.String("h", "localhost:1337", "host to connect to")
+
+func main(){
+	m := new(master.Master)
+	
+	flag.Parse()
+	
+	master.AddServer(*host)
+	
+	rpc.Register(m)
+	//rpc.HandleHTTP()
+
+	l, e := net.Listen("tcp", ":1338")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	rpc.Accept(l)
+	fmt.Println("done")
+	
+	//http.Serve(l, nil)
+	
+}
