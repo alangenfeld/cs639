2e96fbf8eaaac32a141e967f8e8d0b89464a4920
Kevin Paprocki <kpaprocki@wisc.edu>
2011-03-03 18:22:20 -0600

    messing with interfaces..

--- master/serverHeap.go
+++ master/serverHeap.go
@@ -13,22 +13,22 @@ type server struct {
 
 type serverHeap * vector.Vector
 
-func (s serverHeap) Len() int           { 
-	return Len(s) 
+func (s * serverHeap) Len() int           { 
+	return s.Len() 
 }
 
-func (s serverHeap) Less(i, j int) bool { 
+func (s * serverHeap) Less(i, j int) bool { 
 	si := s.at(i).(*server)
 	sj := s.at(j).(*server)
-	return (si.capacity/Len(si.chunks)) < (sj.capacity/Len(sj.chunks))
+	return (si.capacity/si.chunks.Len()) < (sj.capacity/sj.chunks.Len())
 }
-func (s serverHeap) Swap(i, j int)      {
+func (s * serverHeap) Swap(i, j int)      {
 	 s.Swap(i,j)
 }
-func (s serverHeap) Push(serv * server) {
+func (s * serverHeap) Push(serv * server) {
 	s.Push(server)
 }
-func (s serverHeap) Pop() * server {
+func (s * serverHeap) Pop() * server {
 	return s.Pop().(*server)
 }
 
