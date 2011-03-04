af923689715570822a8b1d0c9e63aaf9fffa5727
Kevin Paprocki <kpaprocki@wisc.edu>
2011-03-03 18:28:22 -0600

    made sure things worked before committing!

--- master/serverHeap.go
+++ master/serverHeap.go
@@ -11,24 +11,26 @@ type server struct {
 	chunks *vector.Vector
 }
 
-type serverHeap * vector.Vector
+type serverHeap struct {
+	vec * vector.Vector
+}
 
 func (s * serverHeap) Len() int           { 
-	return s.Len() 
+	return s.vec.Len() 
 }
 
 func (s * serverHeap) Less(i, j int) bool { 
-	si := s.at(i).(*server)
-	sj := s.at(j).(*server)
-	return (si.capacity/si.chunks.Len()) < (sj.capacity/sj.chunks.Len())
+	si := s.vec.At(i).(*server)
+	sj := s.vec.At(j).(*server)
+	return (si.capacity/uint(si.chunks.Len())) < (sj.capacity/uint(sj.chunks.Len()))
 }
 func (s * serverHeap) Swap(i, j int)      {
-	 s.Swap(i,j)
+	 s.vec.Swap(i,j)
 }
 func (s * serverHeap) Push(serv * server) {
-	s.Push(server)
+	s.vec.Push(serv)
 }
 func (s * serverHeap) Pop() * server {
-	return s.Pop().(*server)
+	return s.vec.Pop().(*server)
 }
 
