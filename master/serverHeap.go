a8f60c64750680738358578feceded0a49d3a017
Kevin Paprocki <kpaprocki@wisc.edu>
2011-03-03 18:13:24 -0600

    changed ServerHeap to serverHeap

--- master/serverHeap.go
+++ master/serverHeap.go
@@ -11,24 +11,24 @@ type server struct {
 	chunks *vector.Vector
 }
 
-type ServerHeap * vector.Vector
+type serverHeap * vector.Vector
 
-func (s ServerHeap) Len() int           { 
+func (s serverHeap) Len() int           { 
 	return Len(s) 
 }
 
-func (s ServerHeap) Less(i, j int) bool { 
+func (s serverHeap) Less(i, j int) bool { 
 	si := s.at(i).(*server)
 	sj := s.at(j).(*server)
 	return (si.capacity/Len(si.chunks)) < (sj.capacity/Len(sj.chunks))
 }
-func (s ServerHeap) Swap(i, j int)      {
+func (s serverHeap) Swap(i, j int)      {
 	 s.Swap(i,j)
 }
-func (s ServerHeap) Push(serv * server) {
+func (s serverHeap) Push(serv * server) {
 	s.Push(server)
 }
-func (s ServerHeap) Pop() * server {
+func (s serverHeap) Pop() * server {
 	return s.Pop().(*server)
 }
 
