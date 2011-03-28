86a32e26c389b0d6ca80cf8697383b4cc660e305
Alex Langenfeld <alangenfeld@wisc.edu>
2011-03-28 18:20:49 -0500

    Fix the .sh test scripts

--- simple_test.sh
+++ simple_test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+ssh='ssh -o StrictHostKeyChecking=no '
+
 pwd=`pwd`
-ssh mumble-01 "$pwd/master/master"&
+$ssh mumble-01 "$pwd/master/master"&
 sleep 2
-ssh mumble-40 "$pwd/chunk/serv mumble-01"&
-ssh mumble-39 "$pwd/chunk/serv mumble-01"&
-ssh mumble-38 "$pwd/chunk/serv mumble-01"&
+$ssh mumble-40 "$pwd/chunk/serv mumble-01"&
+$ssh mumble-39 "$pwd/chunk/serv mumble-01"&
+$ssh mumble-38 "$pwd/chunk/serv mumble-01"&
 sleep 2
 client/client mumble-01
 
