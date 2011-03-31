e5d860164bb992664d7bcee445d79438929becaa
Garret Germain <germain@mumble-24.cs.wisc.edu>
2011-03-30 21:54:06 -0500

    fixed write again

--- client/client.go
+++ client/client.go
@@ -156,6 +156,9 @@ func Write (fd int , data []byte  ) (int){
 		toWrite.Data[int(indexWithinChunk)] = data[i]
 		indexWithinChunk++
 		if (indexWithinChunk == sfs.CHUNK_SIZE || indexWithinChunk == len(data)-1 ){
+			if(fdFile.chunkInfo.At(chunkOffset).(sfs.ChunkInfo).ChunkID ==0  ){
+                 fdFile.chunkInfo.Push(AddChunks(fdFile.name, 1))
+            }
 			if((i != fdFile.chunkInfo.Len()-1)|| (sizeToWrite%sfs.CHUNK_SIZE==0)){
 				fileArgs.Length = sfs.CHUNK_SIZE;
 			}else{
