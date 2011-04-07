369b1a0525dbecd084252db199e35f16b24cf38e
lalkaka <rusty@lalkaka.com>
2011-04-07 17:18:35 -0500

    changed all makefiles to use real targets and dependencies. './build_all.sh' on its own does a build w/o a clean; if you want a clean build, './build_all.sh clean' is the ticket

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-(cd include && make clean && make)
-(cd master && make clean && make)&
-(cd client && make clean && make)&
-(cd logger && make clean && make && cd ../chunk && make clean && make)&
+CLEAN=""
+
+if [ "$1" == "clean" ]; then
+	CLEAN="clean-all"
+fi
+
+(cd include && make $CLEAN)
+(cd master && make $CLEAN)&
+(cd client && make $CLEAN)&
+(cd logger && make $CLEAN && cd ../chunk && make $CLEAN)&
 
 wait
