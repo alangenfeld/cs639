eb3e25769aec848db9ff766336a6ef001187ea20
lalkaka <rusty@lalkaka.com>
2011-04-07 16:55:03 -0500

    made our forlorn lil' build_script much faster by parallelizing the build.

--- build_all.sh
+++ build_all.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 
-cd include;
-make clean;
-make;
-cd ../master;
-make clean;
-make;
-cd ../chunk;
-make clean;
-make;
-cd ../client;
-make clean;
-make;
-cd ../logger;
-make clean;
-make;
+(cd include && make clean && make)
+(cd master && make clean && make)&
+(cd client && make clean && make)&
+(cd logger && make clean && make && cd ../chunk && make clean && make)&
+
+wait
