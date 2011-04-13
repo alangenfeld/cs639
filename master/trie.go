26f42e1ba7eb7d4a2d5d03a639f707e904fcc6d6
Kevin Paprocki <kpaprocki@wisc.edu>
2011-04-13 18:42:34 -0500

    added file directory stuff, not final but it hopefully won't break anything

--- master/trie.go
+++ master/trie.go
@@ -52,6 +52,8 @@ import (
 	"utf8"
 	"sort"
 	"fmt"
+	"strconv"
+	"path"
 )
 
 // A Trie uses runes rather than characters for indexing, therefore its child key values are integers.
@@ -61,6 +63,14 @@ type Trie struct {
 	files map[string] string
 	dirs  * vector.Vector
 	children map[int]*Trie // a map if len(s) == 0 {
+}
+
+type inode struct {
+	name        string
+	permissions uint64
+	size        uint64
+	lock        bool
+	chunks      *vector.Vector
 }
 
 // Creates and returns a new Trie instance.
@@ -107,7 +117,191 @@ func (p *Trie) outputDot(vec *vector.StringVector, rune int, depth int) {
 		childNode.outputDot(vec, childRune, depth + 1)
 	}
 }
+// NEEDS FUNCTION TO VALIDATE path_s SYNTAX
+func (p *Trie) AddFile(path_s string) bool{
+	if len(path_s) == 0 {
+		return false
+	}
+	
+	dir, file := path.Split(path_s)
+	
+	// append the runes to the trie
+	leaf := p.includes(strings.NewReader(dir))
+	if leaf == nil{
+		return false
+	}
+    inode , check := leaf.files[file]
+	if check && inode != "" {
+		//file already exists
+		return false
+	} else {
+		leaf.files[file] = file
+	}
+	return true
 
+
+}
+func (p *Trie) DeleteFile(path_s string) bool{
+	if len(path_s) == 0{
+		return false
+	}
+	
+	dir, file := path.Split(path_s)
+	
+	leaf := p.includes(strings.NewReader(dir))
+	if leaf == nil{
+		return false
+	}
+	inode , check := leaf.files[file]
+	if !check || inode == "" {
+		//file doesn't exist.. (maybe don't return?)
+		return false
+	}
+	leaf.files[file] = ""
+	
+	//DELETE INODE IN MASTER???
+	
+	return true
+
+
+
+}
+func (p *Trie) GetFile(path_s string) (inode string, r bool) {
+	if len(path_s) == 0 {
+		return "", false
+	}
+	
+	dir, file := path.Split(path_s)
+	
+	leaf := p.includes(strings.NewReader(dir))
+	if leaf == nil {
+		//bad path_s
+		return "", false
+	}	
+	inode, check := leaf.files[file]
+	if !check || inode == ""{
+		//bad file name
+		return "", false
+	}
+	return leaf.files[file], true
+}
+func (p *Trie) AddDir(path_s string) bool {
+	if len(path_s) == 0 {
+		return false
+	}
+	
+	directory_s, dir_name:= path.Split(path_s)
+	
+	//check to make sure the dir doesn't already exist
+	leaf_test := p.includes(strings.NewReader(path_s))
+	if leaf_test != nil {
+		//dir already exists
+		return false
+	
+	}
+	//create the dir
+	p.addRunes(strings.NewReader(path_s))
+	
+	fmt.Printf("%s\n", directory_s)
+	
+	//add dir record to parent dir
+	dir := p.includes(strings.NewReader(directory_s))
+	if dir == nil {
+		return false
+	}
+	dir.dirs.Push(dir_name)
+
+	return true
+
+}
+func (p *Trie) ReadDir(path_s string) (dirs * vector.Vector, files map[string] string, r bool) {
+	if len(path_s) == 0 {
+		return nil, nil, false
+	}
+	path_cor := fmt.Sprintf("%s%s", path_s, "/")
+	//CHECK path_s SYNTAX to make sure it ends on a forward slash..
+	leaf := p.includes(strings.NewReader(path_cor))
+	if leaf == nil {
+		//bad path_s
+		return nil, nil, false
+	}
+	if leaf.dirs.Len() == 0 {
+		return nil, nil, false
+	}
+	//TRAVERSE FILES STRUCTURE??
+	return leaf.dirs, leaf.files, true
+
+}
+func (p *Trie) MoveDir(oldpath_s string, newpath_s string) bool {
+	movingnode := p.includes(strings.NewReader(oldpath_s))
+	if movingnode == nil {
+		//invalid old path_s
+		return false
+	}
+	//parse new path_s and old to get dir nodes
+	//also get nodes to insert (1 char less than full path_s)
+	//and the name keys of the new and old nodes..
+	new_dir_s := newpath_s //parse..
+	old_dir_s := oldpath_s //parse..
+	new_parent_s := newpath_s //parse..
+	old_parent_s := oldpath_s //parse..
+	old_key := oldpath_s //parse..
+	new_key := newpath_s //parse..
+	old_name := oldpath_s //parse..
+	new_name := newpath_s //parse..
+	newparent := p.addRunes(strings.NewReader(new_parent_s))
+	oldparent := p.includes(strings.NewReader(old_parent_s))
+	if oldparent == nil {
+		//what???
+		return false
+	}
+	newdir := p.includes(strings.NewReader(new_dir_s))
+	if newdir == nil {
+		//what the what??
+		return false
+	}
+	
+	//uniqueness check in newdir..
+	new_length := newdir.dirs.Len()
+	for i := 0 ; i < new_length ; i++ {
+		if newdir.dirs.At(i).(string) == new_name {
+			//failed uniquness check
+			return false
+		}
+	}
+	
+	
+	olddir := p.includes(strings.NewReader(old_dir_s))
+	if olddir == nil {
+		//lies
+		return false
+	}
+	
+	//first add movingnode to new parent
+	key1,_ := strconv.Atoi(new_key)
+	newparent.children[key1] = movingnode
+	
+	//then add it to the new directory
+	newdir.dirs.Push(new_name)
+	
+	//then remove it from the old parent
+	key2,_ := strconv.Atoi(old_key)
+	oldparent.children[key2] = nil
+	if(oldparent.dirs.Len() == 0){
+		oldparent.leaf = true
+	}
+	
+	//then remove it from the old directory (expensive)
+	old_length := olddir.dirs.Len()
+	for i := 0 ; i < old_length ; i++ {
+		if olddir.dirs.At(i).(string) == old_name {
+			olddir.dirs.Delete(i)
+		}
+	}
+	return true
+	
+
+}
 // Internal function: adds items to the trie, reading runes from a strings.Reader.  It returns
 // the leaf node at which the addition ends.
 func (p *Trie) addRunes(r *strings.Reader) *Trie {
@@ -148,17 +342,7 @@ func (p *Trie) AddValue(s string, v interface{}) {
 	leaf := p.addRunes(strings.NewReader(s))
 	leaf.value = v
 }
-func (p *Trie) AddFile(s string, i string) {
-	if len(s) == 0 {
-		return
-	}
 
-	// append the runes to the trie
-	leaf := p.addRunes(strings.NewReader(s))
-	leaf.files[i] = i;
-
-
-}
 // Internal string removal function.  Returns true if this node is empty following the removal.
 func (p *Trie) removeRunes(r *strings.Reader) bool {
 	rune, _, err := r.ReadRune()
