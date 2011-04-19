87f02482e1048097d54404c88daf3f22023bb61d
Tyler Harter <harter@mumble-37.cs.wisc.edu>
2011-04-18 21:32:37 -0500

    another directory test

---
+++ tests/t24.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"../client/client"
+	"fmt"
+	"flag"
+	"os"
+	"sort"
+	"strings"
+)
+
+func createPath(path string) {
+	fd := client.Open(path, client.O_WRONLY|client.O_CREATE)
+	if(fd < 0) {
+		panic("open failed")
+	}
+	ret := client.Close(fd)
+	if(ret != client.WIN) {
+		panic("close failed")
+	}
+}
+
+func main(){
+	master := flag.String("m", "", "specify a master!")
+	flag.Parse();
+
+	client.Initialize(*master)
+
+
+	ls, err := client.ReadDir("/");
+
+	if(err != 0) {
+		panic("readdir should work")
+	}
+	if(len(ls) != 0) {
+		panic("readdir returned wrong number of results")
+	}
+
+
+	if(client.MakeDir("/a") != 0) {
+		panic("makedir should work")
+	}
+
+	fd := client.Open("b", client.O_WRONLY|client.O_CREATE)
+	if(fd < 0) {
+		panic("open failed")
+	}
+
+	//do read dir
+	ls, err = client.ReadDir("/")
+	if(err != 0) {
+		panic("readdir failed")
+	}
+	lsExpected := strings.Split(":","a/:b",-1)
+	sort.SortStrings(lsExpected)
+	sort.SortStrings(ls)
+	if(! ArrEquals(ls, lsExpected)) {
+		panic("readir results differ")
+	}
+
+
+
+	fmt.Printf("\n{{{{{pass}}}}}\n")
+	os.Exit(0)
+}
+
+
+
+
+func ArrEquals(a []string, b []string) bool { 
+	if(len(a) != len(b)) {
+		return false
+	}
+	for i:=0; i < len(a); i++ {
+		if(a[i] != b[i]) {
+			return false
+		}
+	}
+	return true
+}
