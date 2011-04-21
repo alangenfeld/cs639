59df7116ab69ffaf2500b05511089ead31bec8f4
RajaRamYadhav Ramakrishnan <rajaramyadhav@gmail.com>
2011-04-20 19:55:09 -0500

    Corrected mistake in simple_test.sh

--- simple_test.sh
+++ simple_test.sh
@@ -8,11 +8,11 @@ echo "##########################################################"
 sleep 2
 
 pwd=`pwd`
-$ssh mumble-01 "$pwd/master/proxy 1000000000"&
+$ssh mumble-01 "$pwd/master/proxy 1000000000 100"&
 sleep 2
-$ssh mumble-40 "$pwd/chunk/cproxy mumble-01 1000000000"&
-$ssh mumble-39 "$pwd/chunk/cproxy mumble-01 1000000000"&
-$ssh mumble-38 "$pwd/chunk/cproxy mumble-01 1000000000"&
+$ssh mumble-40 "$pwd/chunk/cproxy mumble-01 1000000000 100"&
+$ssh mumble-39 "$pwd/chunk/cproxy mumble-01 1000000000 100"&
+$ssh mumble-38 "$pwd/chunk/cproxy mumble-01 1000000000 100"&
 sleep 2
 client/test mumble-01
 
