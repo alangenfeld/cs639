64e522effeaeb979a906e15fb26800b2cd997518
lalkaka <rusty@lalkaka.com>
2011-04-22 19:49:51 -0500

    modified gen_dir_test to expect trailing slashes on dir names

--- tests/t25.go
+++ tests/t25.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -9,288 +10,286 @@ import (
 	"strings"
 )
 
-func main() {
+func main(){
 	master := flag.String("m", "", "specify a master!")
-	flag.Parse()
+	flag.Parse();
 
 	client.Initialize(*master)
 	var buf []byte
 	buf = buf
 	var ret int
 	ret = ret
-	var ls []string
-	ls = ls
-	var lsExpected []string
-	lsExpected = lsExpected
-	/*
-	TREE:
-	/
-	/La3LFT/
-	/w9FouU/
-	/w9FouU/67iAM_/
-	/La3LFT/Z4CuPz/
-	/La3LFT/pQZOdo/
-	/La3LFT/pQZOdo/kL3PUG/
-	/La3LFT/pQZOdo/njHo3G/
-	/La3LFT/pQZOdo/r4Z4Hb/
-	/La3LFT/pQZOdo/kL3PUG/JInFKn/
-	/La3LFT/pQZOdo/njHo3G/NSSZkg/
-	*/
+	var ls []string; ls = ls
+	var lsExpected []string; lsExpected = lsExpected
+/*
+TREE:
+/
+/HKBFBe/
+/YisZFh/
+/HKBFBe/xA9o_c/
+/HKBFBe/9O1cEa/
+/YisZFh/TWciId/
+/YisZFh/fmCxop/
+/YisZFh/GRro57/
+/YisZFh/J_PhjQ/
+/YisZFh/_wIEZz/
+/YisZFh/_wIEZz/lUGH0D/
+*/
 
-	ret = client.MakeDir("/La3LFT/")
-	if ret != 0 {
-		panic("MakeDir failed")
-	}
+ret = client.MakeDir("/HKBFBe/")
+if(ret != 0) {
+  panic("MakeDir failed")
+}
 
-	ret = client.MakeDir("/w9FouU/")
-	if ret != 0 {
-		panic("MakeDir failed")
-	}
+ret = client.MakeDir("/YisZFh/")
+if(ret != 0) {
+  panic("MakeDir failed")
+}
 
-	ret = client.MakeDir("/w9FouU/67iAM_/")
-	if ret != 0 {
-		panic("MakeDir failed")
-	}
+ret = client.MakeDir("/HKBFBe/xA9o_c/")
+if(ret != 0) {
+  panic("MakeDir failed")
+}
 
-	ret = client.MakeDir("/La3LFT/Z4CuPz/")
-	if ret != 0 {
-		panic("MakeDir failed")
-	}
+ret = client.MakeDir("/HKBFBe/9O1cEa/")
+if(ret != 0) {
+  panic("MakeDir failed")
+}
 
-	ret = client.MakeDir("/La3LFT/pQZOdo/")
-	if ret != 0 {
-		panic("MakeDir failed")
-	}
+ret = client.MakeDir("/YisZFh/TWciId/")
+if(ret != 0) {
+  panic("MakeDir failed")
+}
 
-	ret = client.MakeDir("/La3LFT/pQZOdo/kL3PUG/")
-	if ret != 0 {
-		panic("MakeDir failed")
-	}
+ret = client.MakeDir("/YisZFh/fmCxop/")
+if(ret != 0) {
+  panic("MakeDir failed")
+}
 
-	ret = client.MakeDir("/La3LFT/pQZOdo/njHo3G/")
-	if ret != 0 {
-		panic("MakeDir failed")
-	}
+ret = client.MakeDir("/YisZFh/GRro57/")
+if(ret != 0) {
+  panic("MakeDir failed")
+}
 
-	ret = client.MakeDir("/La3LFT/pQZOdo/r4Z4Hb/")
-	if ret != 0 {
-		panic("MakeDir failed")
-	}
+ret = client.MakeDir("/YisZFh/J_PhjQ/")
+if(ret != 0) {
+  panic("MakeDir failed")
+}
 
-	ret = client.MakeDir("/La3LFT/pQZOdo/kL3PUG/JInFKn/")
-	if ret != 0 {
-		panic("MakeDir failed")
-	}
+ret = client.MakeDir("/YisZFh/_wIEZz/")
+if(ret != 0) {
+  panic("MakeDir failed")
+}
 
-	ret = client.MakeDir("/La3LFT/pQZOdo/njHo3G/NSSZkg/")
-	if ret != 0 {
-		panic("MakeDir failed")
-	}
+ret = client.MakeDir("/YisZFh/_wIEZz/lUGH0D/")
+if(ret != 0) {
+  panic("MakeDir failed")
+}
 
-	createPath("/rkE42a.txt")
+createPath("/YisZFh/_wIEZz/Gj46DI.txt")
 
-	createPath("/La3LFT/pQZOdo/kL3PUG/gzOmTW.txt")
+createPath("/HKBFBe/9O1cEa/bTg0uN.txt")
 
-	createPath("/La3LFT/pQZOdo/kL3PUG/RDA6ux.txt")
+createPath("/1mf6P2.txt")
 
-	createPath("/La3LFT/jgJiEV.txt")
+createPath("/YisZFh/TWciId/6CBF7G.txt")
 
-	createPath("/Mcn_0P.txt")
+createPath("/YisZFh/J_PhjQ/0s4gx2.txt")
 
-	createPath("/La3LFT/pQZOdo/njHo3G/NSSZkg/p9LNpw.txt")
+createPath("/HKBFBe/8aBbjb.txt")
 
-	createPath("/La3LFT/pQZOdo/x0fRwt.txt")
+createPath("/YisZFh/fmCxop/pHoiDi.txt")
 
-	createPath("/La3LFT/Z4CuPz/VdAcKu.txt")
+createPath("/54EkC7.txt")
 
-	createPath("/bUl52u.txt")
+createPath("/HKBFBe/_S_mGz.txt")
 
-	createPath("/w9FouU/67iAM_/GxhNt0.txt")
+createPath("/YisZFh/H2OC3g.txt")
 
-	createPath("/La3LFT/pQZOdo/njHo3G/NSSZkg/bDgIwU.txt")
+createPath("/YisZFh/GRro57/ReMWv5.txt")
 
-	createPath("/La3LFT/Z4CuPz/tj5hfp.txt")
+createPath("/HKBFBe/4eYB6n.txt")
 
-	createPath("/La3LFT/pQZOdo/kL3PUG/JInFKn/m4Nm2S.txt")
+createPath("/HKBFBe/xA9o_c/wz8XZV.txt")
 
-	createPath("/w9FouU/67iAM_/cR_oUB.txt")
+createPath("/YisZFh/fmCxop/HzdYke.txt")
 
-	createPath("/La3LFT/pQZOdo/njHo3G/xoj_h9.txt")
+createPath("/HKBFBe/xA9o_c/hTgTyW.txt")
 
-	createPath("/La3LFT/pQZOdo/kL3PUG/JInFKn/xkHUZh.txt")
+createPath("/YisZFh/TWciId/_wfEZr.txt")
 
-	createPath("/La3LFT/pQZOdo/njHo3G/w3m6rq.txt")
+createPath("/YisZFh/_wIEZz/Yomrr6.txt")
 
-	createPath("/La3LFT/pQZOdo/kL3PUG/JInFKn/jthx9V.txt")
+createPath("/HKBFBe/xA9o_c/cRxezL.txt")
 
-	createPath("/w9FouU/t0Rc1T.txt")
+createPath("/YisZFh/J_PhjQ/QI0UTJ.txt")
 
-	createPath("/La3LFT/pQZOdo/r4Z4Hb/gQ_aYr.txt")
+createPath("/YisZFh/fmCxop/wwSkkm.txt")
 
-	lsExpected = []string{"La3LFT", "w9FouU", "rkE42a.txt", "Mcn_0P.txt", "bUl52u.txt"}
+lsExpected = []string{"YisZFh/","HKBFBe/","1mf6P2.txt","54EkC7.txt"}
 
-	ls, ret = client.ReadDir("/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	sort.SortStrings(lsExpected)
-	sort.SortStrings(ls)
-	if !ArrEquals(ls, lsExpected) {
-		fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls)
-		panic("Unexpected ReadDir results")
-	}
+ls, ret = client.ReadDir("/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+sort.SortStrings(lsExpected)
+sort.SortStrings(ls)
+if(! ArrEquals(ls, lsExpected)) {
+  fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls);
+  panic("Unexpected ReadDir results")
+}
 
-	lsExpected = []string{"pQZOdo", "Z4CuPz", "jgJiEV.txt"}
+lsExpected = []string{"xA9o_c/","9O1cEa/","8aBbjb.txt","_S_mGz.txt","4eYB6n.txt"}
 
-	ls, ret = client.ReadDir("/La3LFT/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	sort.SortStrings(lsExpected)
-	sort.SortStrings(ls)
-	if !ArrEquals(ls, lsExpected) {
-		fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls)
-		panic("Unexpected ReadDir results")
-	}
+ls, ret = client.ReadDir("/HKBFBe/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+sort.SortStrings(lsExpected)
+sort.SortStrings(ls)
+if(! ArrEquals(ls, lsExpected)) {
+  fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls);
+  panic("Unexpected ReadDir results")
+}
 
-	lsExpected = []string{"67iAM_", "t0Rc1T.txt"}
+lsExpected = []string{"_wIEZz/","GRro57/","fmCxop/","TWciId/","J_PhjQ/","H2OC3g.txt"}
 
-	ls, ret = client.ReadDir("/w9FouU/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	sort.SortStrings(lsExpected)
-	sort.SortStrings(ls)
-	if !ArrEquals(ls, lsExpected) {
-		fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls)
-		panic("Unexpected ReadDir results")
-	}
+ls, ret = client.ReadDir("/YisZFh/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+sort.SortStrings(lsExpected)
+sort.SortStrings(ls)
+if(! ArrEquals(ls, lsExpected)) {
+  fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls);
+  panic("Unexpected ReadDir results")
+}
 
-	lsExpected = []string{"GxhNt0.txt", "cR_oUB.txt"}
+lsExpected = []string{"wz8XZV.txt","hTgTyW.txt","cRxezL.txt"}
 
-	ls, ret = client.ReadDir("/w9FouU/67iAM_/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	sort.SortStrings(lsExpected)
-	sort.SortStrings(ls)
-	if !ArrEquals(ls, lsExpected) {
-		fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls)
-		panic("Unexpected ReadDir results")
-	}
+ls, ret = client.ReadDir("/HKBFBe/xA9o_c/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+sort.SortStrings(lsExpected)
+sort.SortStrings(ls)
+if(! ArrEquals(ls, lsExpected)) {
+  fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls);
+  panic("Unexpected ReadDir results")
+}
 
-	lsExpected = []string{"VdAcKu.txt", "tj5hfp.txt"}
+lsExpected = []string{"bTg0uN.txt"}
 
-	ls, ret = client.ReadDir("/La3LFT/Z4CuPz/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	sort.SortStrings(lsExpected)
-	sort.SortStrings(ls)
-	if !ArrEquals(ls, lsExpected) {
-		fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls)
-		panic("Unexpected ReadDir results")
-	}
+ls, ret = client.ReadDir("/HKBFBe/9O1cEa/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+sort.SortStrings(lsExpected)
+sort.SortStrings(ls)
+if(! ArrEquals(ls, lsExpected)) {
+  fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls);
+  panic("Unexpected ReadDir results")
+}
 
-	lsExpected = []string{"r4Z4Hb", "kL3PUG", "njHo3G", "x0fRwt.txt"}
+lsExpected = []string{"6CBF7G.txt","_wfEZr.txt"}
 
-	ls, ret = client.ReadDir("/La3LFT/pQZOdo/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	sort.SortStrings(lsExpected)
-	sort.SortStrings(ls)
-	if !ArrEquals(ls, lsExpected) {
-		fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls)
-		panic("Unexpected ReadDir results")
-	}
+ls, ret = client.ReadDir("/YisZFh/TWciId/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+sort.SortStrings(lsExpected)
+sort.SortStrings(ls)
+if(! ArrEquals(ls, lsExpected)) {
+  fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls);
+  panic("Unexpected ReadDir results")
+}
 
-	lsExpected = []string{"JInFKn", "gzOmTW.txt", "RDA6ux.txt"}
+lsExpected = []string{"pHoiDi.txt","HzdYke.txt","wwSkkm.txt"}
 
-	ls, ret = client.ReadDir("/La3LFT/pQZOdo/kL3PUG/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	sort.SortStrings(lsExpected)
-	sort.SortStrings(ls)
-	if !ArrEquals(ls, lsExpected) {
-		fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls)
-		panic("Unexpected ReadDir results")
-	}
+ls, ret = client.ReadDir("/YisZFh/fmCxop/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+sort.SortStrings(lsExpected)
+sort.SortStrings(ls)
+if(! ArrEquals(ls, lsExpected)) {
+  fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls);
+  panic("Unexpected ReadDir results")
+}
 
-	lsExpected = []string{"NSSZkg", "xoj_h9.txt", "w3m6rq.txt"}
+lsExpected = []string{"ReMWv5.txt"}
 
-	ls, ret = client.ReadDir("/La3LFT/pQZOdo/njHo3G/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	sort.SortStrings(lsExpected)
-	sort.SortStrings(ls)
-	if !ArrEquals(ls, lsExpected) {
-		fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls)
-		panic("Unexpected ReadDir results")
-	}
+ls, ret = client.ReadDir("/YisZFh/GRro57/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+sort.SortStrings(lsExpected)
+sort.SortStrings(ls)
+if(! ArrEquals(ls, lsExpected)) {
+  fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls);
+  panic("Unexpected ReadDir results")
+}
 
-	lsExpected = []string{"gQ_aYr.txt"}
+lsExpected = []string{"0s4gx2.txt","QI0UTJ.txt"}
 
-	ls, ret = client.ReadDir("/La3LFT/pQZOdo/r4Z4Hb/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	sort.SortStrings(lsExpected)
-	sort.SortStrings(ls)
-	if !ArrEquals(ls, lsExpected) {
-		fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls)
-		panic("Unexpected ReadDir results")
-	}
+ls, ret = client.ReadDir("/YisZFh/J_PhjQ/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+sort.SortStrings(lsExpected)
+sort.SortStrings(ls)
+if(! ArrEquals(ls, lsExpected)) {
+  fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls);
+  panic("Unexpected ReadDir results")
+}
 
-	lsExpected = []string{"m4Nm2S.txt", "xkHUZh.txt", "jthx9V.txt"}
+lsExpected = []string{"lUGH0D/","Gj46DI.txt","Yomrr6.txt"}
 
-	ls, ret = client.ReadDir("/La3LFT/pQZOdo/kL3PUG/JInFKn/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	sort.SortStrings(lsExpected)
-	sort.SortStrings(ls)
-	if !ArrEquals(ls, lsExpected) {
-		fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls)
-		panic("Unexpected ReadDir results")
-	}
+ls, ret = client.ReadDir("/YisZFh/_wIEZz/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+sort.SortStrings(lsExpected)
+sort.SortStrings(ls)
+if(! ArrEquals(ls, lsExpected)) {
+  fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls);
+  panic("Unexpected ReadDir results")
+}
 
-	lsExpected = []string{"p9LNpw.txt", "bDgIwU.txt"}
+lsExpected = []string{}
 
-	ls, ret = client.ReadDir("/La3LFT/pQZOdo/njHo3G/NSSZkg/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	sort.SortStrings(lsExpected)
-	sort.SortStrings(ls)
-	if !ArrEquals(ls, lsExpected) {
-		fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls)
-		panic("Unexpected ReadDir results")
-	}
+ls, ret = client.ReadDir("/YisZFh/_wIEZz/lUGH0D/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+sort.SortStrings(lsExpected)
+sort.SortStrings(ls)
+if(! ArrEquals(ls, lsExpected)) {
+  fmt.Printf("For ReadDir we expected %v but got %v\n", lsExpected, ls);
+  panic("Unexpected ReadDir results")
+}
 
-	recursiveDelete("/")
-	ls, ret = client.ReadDir("/")
-	if ret != 0 {
-		panic("ReadDir failed")
-	}
-	if len(ls) != 0 {
-		panic("after a recursive delete, everything should be gone")
-	}
+recursiveDelete("/")
+ls, ret = client.ReadDir("/")
+if(ret != 0) {
+  panic("ReadDir failed")
+}
+if(len(ls) != 0) {
+  panic("after a recursive delete, everything should be gone")
+}
 
 	fmt.Printf("\n{{{{{pass}}}}}\n")
 	os.Exit(0)
 }
 
 
-func ArrEquals(a []string, b []string) bool {
-	if len(a) != len(b) {
+func ArrEquals(a []string, b []string) bool { 
+	if(len(a) != len(b)) {
 		return false
 	}
-
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
+	
+	for i:=0; i < len(a); i++ {
+		if(a[i] != b[i]) {
 			return false
 		}
 	}
@@ -300,27 +299,28 @@ func ArrEquals(a []string, b []string) bool {
 
 func createPath(path string) {
 	fd := client.Open(path, client.O_WRONLY|client.O_CREATE)
-	if fd < 0 {
+	if(fd < 0) {
 		panic("open failed")
 	}
 	ret := client.Close(fd)
-	if ret != client.WIN {
+	if(ret != client.WIN) {
 		panic("close failed")
 	}
 }
+
 
 
 func recursiveDelete(path string) {
 	fmt.Printf("Recursive delete on %s\n", path)
 
 	ls, ret := client.ReadDir(path)
-	if ret != 0 {
+	if(ret != 0) {
 		panic("ReadDir failed")
 	}
-	if len(ls) == 0 {
-		if path != "/" {
-			ret = client.RemoveDir(path)
-			if ret != 0 {
+	if(len(ls) == 0) {
+		if(path != "/") {
+			ret = client.RemoveDir(path);
+			if(ret != 0) {
 				panic("RemoveDir failed")
 			}
 		}
@@ -328,27 +328,27 @@ func recursiveDelete(path string) {
 	}
 
 	//try deleting it non-empty
-	if client.RemoveDir(path) == 0 {
+	if(client.RemoveDir(path) == 0) {
 		panic("it let us delete a non-empty directory!")
 	}
 
 	//we have to recursive delete stuff
-	for i := 0; i < len(ls); i++ {
-		if strings.HasSuffix(ls[i], "/") {
+	for i:=0; i<len(ls); i++ {
+		if(strings.HasSuffix(ls[i],"/")) {
 			//its a dir, do recursive
-			recursiveDelete(path + ls[i])
+			recursiveDelete(path+ls[i])
 		} else {
 			//its a file, delete it
-			if client.Delete(path+ls[i]) != 0 {
+			if(client.Delete(path+ls[i]) != 0) {
 				fmt.Printf("Try to delete %s\n", path+ls[i])
-				panic("Couldnt delete file")
+				panic("Couldnt delete file");
 			}
 		}
 	}
 
 	//try deleting it empty
-	if path != "/" {
-		if client.RemoveDir(path) != 0 {
+	if(path != "/") {
+		if(client.RemoveDir(path) != 0) {
 			panic("couldnt delete directory!")
 		}
 	}
